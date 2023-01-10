8b71ab66f690545cc1c76f5f3f255a153922e9c7
gogabarnabishvili <99512619+gogabarnabishvili@users.noreply.github.com>
2023-01-11 03:18:33 +0400

    need more burger animation

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+const responseNav = document.querySelector(".headerNav");
+
+function burgerManu(e) {
+    responseNav.classList.toggle("visibilityHidden");
+
+    // responseNav.classList.toggle("animation");
+
+}
