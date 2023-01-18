0af68bf75c2d5b498ce72509a487bdfd2429aeba
gogabarnabishvili <99512619+gogabarnabishvili@users.noreply.github.com>
2023-01-19 02:48:55 +0400

    finish here

--- scrypt.js
+++ scrypt.js
@@ -4,3 +4,6 @@ const responseNav = document.querySelector(".navUl");
 function burgerManu() {
     responseNav.classList.toggle("visibilityHidden");
 }
+responseNav.addEventListener("click", () => {
+    responseNav.classList.remove("visibilityHidden")
+})
